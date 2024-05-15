fd29f54c5101cdd1de45eb4b7afdf9aff48c21e4
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-05-15 09:59:20 -0300

    first commit

---
+++ Blazor.AplicationWeb/Server/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Blazor.AplicationWeb.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.AplicationWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            List<int> teste = new List<int>() { 1, 2, 3 };
+            return teste.ToArray();
+        }
+    }
+}
