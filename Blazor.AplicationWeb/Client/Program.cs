ef46d146289eafe448082d9b9f12c9068401fb65
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-05-15 22:14:12 -0300

    Testando

--- Blazor.AplicationWeb/Client/Program.cs
+++ Blazor.AplicationWeb/Client/Program.cs
@@ -1,11 +1,21 @@
 using Blazor.AplicationWeb.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Adicione a configuração do DbContext dentro do método ConfigureServices
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 await builder.Build().RunAsync();
